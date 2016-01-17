2d42f28afaf4a1ff9e86ce7649b91e2d24b77741
dmygaien <dmygaenko@gmail.com>
2016-01-17 16:51:42 +0200

    Fixed join query

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 drop table if EXISTS city;
-drop table if EXISTS author;
-drop table if EXISTS author_address;
+--drop table if EXISTS author;
+--drop table if EXISTS author_address;
 drop table if EXISTS country;
 
 --  city table
@@ -12,7 +12,7 @@ insert into city (id, city_type, name, country_id1, country_id2, country_name) v
 insert into city (id, city_type, name, country_id1, country_id2, country_name) values (4, 3, 'Lviv', 1, 1, 'Ukraine');
 
 --  author table
-create TABLE author (id int PRIMARY KEY, name varchar(255), date_of_birth date);
+/*create TABLE author (id int PRIMARY KEY, name varchar(255), date_of_birth date);
 
 insert into author (id, name, date_of_birth) values (1, 'Dmytro', '1990-09-11');
 insert into author (id, name, date_of_birth) values (2, 'Sergiy', '1990-02-28');
@@ -21,7 +21,7 @@ insert into author (id, name, date_of_birth) values (2, 'Sergiy', '1990-02-28');
 create TABLE author_address (author_id int PRIMARY KEY, street varchar(255), number int, flat int);
 
 insert into author_address (author_id, street, number, flat) values (1, 'Melnykova', '63', '13');
-insert into author_address (author_id, street, number, flat) values (2, 'Lykyanivka', '1', '128');
+insert into author_address (author_id, street, number, flat) values (2, 'Lykyanivka', '1', '128');*/
 
 --  country table
 create TABLE country (id1 varchar(255), id2 varchar(255), name varchar(255), code1 varchar(255), code2 varchar(255), PRIMARY KEY(id1, id2));
