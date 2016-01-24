90f36f13fd4affcb7412357dcc42def001ace65b
dmygaien <dmygaenko@gmail.com>
2016-01-24 21:51:31 +0200

    added elasticsearch module

---
+++ practice-elacticsearch/src/main/java/com/mygaienko/practice/elasticsearch/config/ElasticCOnfig.java
@@ -0,0 +1,22 @@
+package com.mygaienko.practice.elasticsearch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+
+/**
+ * Created by enda1n on 24.01.2016.
+ */
+@Configuration
+@EnableElasticsearchRepositories("com.mygaienko.practice.elasticsearch")
+public class ElasticConfig {
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchTemplate(nodeBuilder().local(true).node().client());
+    }
+}
