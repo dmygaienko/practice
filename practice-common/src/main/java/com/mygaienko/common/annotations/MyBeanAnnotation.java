ba536f06ed9ecfdfa4ba712370c5ea057beaf8d5
dmygaenko <dmygaenko@gmail.com>
2016-01-27 19:22:54 +0200

    added commons module

---
+++ practice-common/src/main/java/com/mygaienko/common/annotations/MyBeanAnnotation.java
@@ -0,0 +1,21 @@
+package com.mygaienko.common.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by dmygaenko on 27/01/2016.
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface MyBeanAnnotation {
+
+    String customName() default "";
+
+    int years();
+
+    String[] customNames();
+}
