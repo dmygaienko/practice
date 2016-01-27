c8a14a1bf2f90f6d14ab0be74bc99724b3f4e718
dmygaien <dmygaenko@gmail.com>
2016-01-28 00:38:43 +0200

    create cglib module

---
+++ practice-cglib/src/main/java/com/mygaienko/cglib/model/SampleClass.java
@@ -0,0 +1,12 @@
+package com.mygaienko.cglib.model;
+
+/**
+ * Created by enda1n on 28.01.2016.
+ */
+public class SampleClass {
+
+    public String test(String input) {
+        return "Hello world!";
+    }
+
+}
