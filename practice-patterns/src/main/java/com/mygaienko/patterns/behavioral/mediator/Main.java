43bb5b70e90c4b5a60f485eb79390ab9d6011dc6
dmygaienko <dmygaenko@gmail.com>
2016-03-23 14:11:02 +0200

    added patterns

---
+++ practice-patterns/src/main/java/com/mygaienko/patterns/behavioral/mediator/Main.java
@@ -0,0 +1,7 @@
+package com.mygaienko.patterns.behavioral.mediator;
+
+/**
+ * Created by dmygaenko on 23/03/2016.
+ */
+public class Main {
+}
---
+++ practice-patterns/src/main/java/com/mygaienko/patterns/creational/abstractfactory/Main.java
@@ -0,0 +1,7 @@
+package com.mygaienko.patterns.creational.abstractfactory;
+
+/**
+ * Created by dmygaenko on 23/03/2016.
+ */
+public class Main {
+}
