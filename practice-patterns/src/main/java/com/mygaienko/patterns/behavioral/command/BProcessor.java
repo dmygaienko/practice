fcccf4cc27a61290d7c502b85c3c72d38c1c7c89
dmygaenko <dmygaenko@gmail.com>
2016-03-24 16:23:25 +0200

    edit command patterns

---
+++ practice-patterns/src/main/java/com/mygaienko/patterns/behavioral/command/BProcessor.java
@@ -0,0 +1,12 @@
+package com.mygaienko.patterns.behavioral.command;
+
+/**
+ * Created by dmygaenko on 24/03/2016.
+ */
+public class BProcessor implements Processor<Context, Result> {
+
+    @Override
+    public Result process(Context context) {
+        return null;
+    }
+}
---
+++ practice-patterns/src/main/java/com/mygaienko/patterns/behavioral/command/Result.java
@@ -0,0 +1,7 @@
+package com.mygaienko.patterns.behavioral.command;
+
+/**
+ * Created by dmygaenko on 24/03/2016.
+ */
+public class Result {
+}
