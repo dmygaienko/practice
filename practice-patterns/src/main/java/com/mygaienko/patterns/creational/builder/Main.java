dedc807d291d05778056c2c933e496eb278dd7d6
dmygaenko <dmygaenko@gmail.com>
2016-03-25 15:38:37 +0200

    added builder pattern

--- practice-patterns/src/main/java/com/mygaienko/patterns/creational/builder/Main.java
+++ practice-patterns/src/main/java/com/mygaienko/patterns/creational/builder/Main.java
@@ -4,4 +4,18 @@ package com.mygaienko.patterns.creational.builder;
  * Created by dmygaenko on 23/03/2016.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        Bean bean = new Bean.BeanBuilder()
+                .setStringA("A")
+                .setStringB("B")
+                .setStringC("C")
+                .setStringD("D")
+                .setStringE("E")
+                .setStringF("F")
+                .setStringG("G")
+                .setStringH("H")
+                .build();
+        System.out.println(bean);
+    }
 }
