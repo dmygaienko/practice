1349e8d2c279752961cebc07ad380ba372005c49
dmygaenko <dmygaenko@gmail.com>
2016-05-31 15:14:08 +0300

    added generics example

---
+++ practice-common/src/main/java/com/mygaienko/common/model/catdog_generics/Animal.java
@@ -0,0 +1,20 @@
+package com.mygaienko.common.model.catdog_generics;
+
+/**
+ * Created by dmygaenko on 31/05/2016.
+ */
+public class Animal implements Cat, Dog {
+
+    public Animal() {
+    }
+
+    @Override
+    public void cat() {
+
+    }
+
+    @Override
+    public void dog() {
+
+    }
+}
