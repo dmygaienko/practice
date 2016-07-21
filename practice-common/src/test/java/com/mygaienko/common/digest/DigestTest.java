c9252ff2f2237c9c3f2d927b78a70d20848388c2
dmygaenko <dmygaenko@gmail.com>
2016-07-21 13:57:32 +0300

    concurrency

--- practice-common/src/test/java/com/mygaienko/common/digest/DigestTest.java
+++ practice-common/src/test/java/com/mygaienko/common/digest/DigestTest.java
@@ -1,10 +1,15 @@
 package com.mygaienko.common.digest;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by dmygaenko on 20/07/2016.
@@ -12,11 +17,31 @@ import java.util.Arrays;
 public class DigestTest {
     
     @Test
-    public void test() throws NoSuchAlgorithmException {
+    public void testDigester() throws NoSuchAlgorithmException {
         MessageDigest digester = MessageDigest.getInstance("SHA-256");
         digester.update("password".getBytes());
 
         System.out.println(Arrays.toString(digester.digest()));
     }
-    
+
+    @Test
+    public void testDigesterWithSalt() throws NoSuchAlgorithmException {
+        MessageDigest digester = MessageDigest.getInstance("SHA-256");
+        digester.update("salt".getBytes());
+        digester.update("password".getBytes());
+
+       // System.out.println(new BigInteger(1, digester.digest()).toString(16));
+        //System.out.println(new BigInteger(digester.digest()).toString(16));
+        System.out.println(DigestUtils.sha256Hex("salt" + "password"));
+    }
+
+    @Test
+    public void testBCryptPasswordEncoder() throws NoSuchAlgorithmException {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encoded = encoder.encode("password");
+
+        assertTrue(encoder.matches("password", encoded));
+    }
+
+
 }
