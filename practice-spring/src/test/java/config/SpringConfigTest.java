c9a76c9f49e4e73c1da75a7640f924d4534fead0
dmygaenko <dmygaenko@gmail.com>
2016-09-14 18:15:13 +0300

    added service locator example

--- practice-spring/src/test/java/config/SpringConfigTest.java
+++ practice-spring/src/test/java/config/SpringConfigTest.java
@@ -1,12 +1,16 @@
 package config;
 
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import service.AutowiredBean;
 import service.BeanB;
+
+import java.lang.reflect.Field;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -31,6 +35,15 @@ public class SpringConfigTest {
     public void testBeanB2() throws Exception {
         BeanB beanB2 = context.getBean("BeanB2", BeanB.class);
         assertTrue(beanB2.getStrings().contains("String4 by my CustomQualifier"));
+        assertNotNull(beanB2.getAutowiredBean());
+    }
+
+    @Test
+    public void testFieldAccessorForBeanB2() throws Exception {
+        BeanB beanB2 = new BeanB(StringUtils.EMPTY);
+        Field field = beanB2.getClass().getDeclaredField("autowiredBean");
+        field.setAccessible(true);
+        field.set(beanB2, new AutowiredBean());
         assertNotNull(beanB2.getAutowiredBean());
     }
 
