2c4b3c271038e5add00e2adb24534246169ef838
dmygaenko <dmygaenko@gmail.com>
2016-09-16 14:27:01 +0300

    added method injection

--- practice-spring/src/test/java/config/PrototypeConfigTest.java
+++ practice-spring/src/test/java/config/PrototypeConfigTest.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.HashSet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by dmygaenko on 16/09/2016.
  */
@@ -20,9 +25,13 @@ public class PrototypeConfigTest {
     @Test
     public void testPrototype() throws Exception {
         System.out.println("testPrototype");
-        System.out.println("prototype id: " + beanC.getPrototype().getId());
-        System.out.println("prototype id: " + beanC.getPrototype().getId());
-        System.out.println("prototype id: " + beanC.getPrototype().getId());
-        System.out.println("prototype id: " + beanC.getPrototype().getId());
+        HashSet<String> ids = new HashSet<String>();
+        assertTrue(ids.add(String.valueOf(beanC.getPrototype().getId())));
+        assertTrue(ids.add(String.valueOf(beanC.getPrototype().getId())));
+        assertTrue(ids.add(String.valueOf(beanC.getPrototype().getId())));
+        assertTrue(ids.add(String.valueOf(beanC.getPrototype().getId())));
+        assertTrue(ids.add(String.valueOf(beanC.getPrototype().getId())));
+        assertEquals(ids.size(), 5);
+        System.out.println(ids);
     }
 }
