ac11f89a7f060262352094173c95ec56847e4408
dmygaenko <dmygaenko@gmail.com>
2016-09-28 16:26:25 +0300

    to commit

---
+++ practice-common/src/test/java/com/mygaienko/common/string/StringUtilsTest.java
@@ -0,0 +1,20 @@
+package com.mygaienko.common.string;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+/**
+ * Created by dmygaenko on 02/06/2016.
+ */
+public class StringUtilsTest {
+
+    @Test
+    public void test() throws Exception {
+        System.out.println(StringUtils.difference("move 1", "mve 1"));
+    }
+
+    @Test
+    public void test1() throws Exception {
+        System.out.println("'" + StringUtils.difference("move 1", " mve 1 ") + "'");
+    }
+}
