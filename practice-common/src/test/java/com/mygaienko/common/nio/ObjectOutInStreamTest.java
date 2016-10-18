224c4c8c94600d2e0c82aaeb24664ffdbe4bdebe
dmygaien <dmygaenko@gmail.com>
2016-10-19 01:31:19 +0300

    to commit

--- practice-common/src/test/java/com/mygaienko/common/nio/ObjectOutInStreamTest.java
+++ practice-common/src/test/java/com/mygaienko/common/nio/ObjectOutInStreamTest.java
@@ -34,8 +34,23 @@ public class ObjectOutInStreamTest {
         }
     }
 
+    @Test
+    public void testExternalizable() throws Exception {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file))) {
+            outputStream.writeObject(getBeanExternalizable(1, '2', "3"));
+        }
+
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file))) {
+            assertThat(inputStream.readObject(), is(getBeanExternalizable(1, '2', "3")));
+        }
+    }
+
     private Bean getBean(int i, char c, String s) {
         return new Bean(i, c, s);
+    }
+
+    private BeanExternalizable getBeanExternalizable(int i, char c, String s) {
+        return new BeanExternalizable(i, c, s);
     }
 
     private static class Bean implements Serializable {
@@ -69,6 +84,72 @@ public class ObjectOutInStreamTest {
             if (this == obj) return true;
 
             final Bean that = (Bean) obj;
+
+            return new EqualsBuilder()
+                    .append(this.i, that.i)
+                    .append(this.c, that.c)
+                    .append(this.s, that.s)
+                    .isEquals();
+        }
+
+        @Override
+        public int hashCode() {
+            int result = i;
+            result = 31 * result + (int) c;
+            result = 31 * result + (s != null ? s.hashCode() : 0);
+            return result;
+        }
+    }
+
+    private static class BeanExternalizable implements Externalizable {
+
+        private int i;
+        private char c;
+        private String s;
+
+        public BeanExternalizable() {
+        }
+
+        public BeanExternalizable(int i, char c, String s) {
+            this.i = i;
+            this.c = c;
+            this.s = s;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public char getC() {
+            return c;
+        }
+
+        public String getS() {
+            return s;
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.write(i);
+            out.writeChar(c);
+            out.writeChars(s);
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            this.i = in.read();
+            this.c = in.readChar();
+            //TODO find how to get string
+            this.s = in.readLine();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof BeanExternalizable)) return false;
+
+            if (this == obj) return true;
+
+            final BeanExternalizable that = (BeanExternalizable) obj;
 
             return new EqualsBuilder()
                     .append(this.i, that.i)
