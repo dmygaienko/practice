45b48646a1b8026c8b44c0cff4b2ccd63fde4740
dmygaien <dmygaenko@gmail.com>
2016-12-24 12:02:58 +0200

    New changelist

---
+++ practice-common/src/test/java/com/mygaienko/common/algorithms/ForkJoinDynamicCutRodTest.java
@@ -0,0 +1,33 @@
+package com.mygaienko.common.algorithms;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by enda1n on 24.12.2016.
+ */
+public class ForkJoinDynamicCutRodTest {
+
+    private final long[] prices = new long[] {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+
+    @Test
+    public void execute2() throws Exception {
+        assertEquals(5, ForkJoinDynamicCutRod.execute(prices, 2));
+    }
+
+    @Test
+    public void execute10() throws Exception {
+        assertEquals(30, ForkJoinDynamicCutRod.execute(prices, 10));
+    }
+
+    @Test
+    public void execute30() throws Exception {
+        assertEquals(90, ForkJoinDynamicCutRod.execute(prices, 30));
+    }
+
+    @Test
+    public void execute40() throws Exception {
+        assertEquals(120, ForkJoinDynamicCutRod.execute(prices, 40));
+    }
+}
