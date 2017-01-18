03fe0d9ee667eec5cf7a2e19996d9a86929cbc68
dmygaenko <dmygaenko@gmail.com>
2017-01-18 15:53:42 +0200

    add loader tests

---
+++ practice-common/src/test/java/com/mygaienko/common/concurrency/CompletableFutureTest.java
@@ -0,0 +1,67 @@
+package com.mygaienko.common.concurrency;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.client.AsyncRestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
+
+/**
+ * Created by dmygaenko on 18/01/2017.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CompletableFutureTest {
+
+    @Mock
+    private AsyncRestTemplate asyncRestTemplate;
+
+    public void test() {
+        ListenableFuture<ResponseEntity<Response>> future1 = asyncRestTemplate.postForEntity("url1", null, Response.class);
+        ListenableFuture<ResponseEntity<Response>> future2 = asyncRestTemplate.postForEntity("url2", null, Response.class);
+
+        CompletableFuture<ResponseEntity<Response>> uCompletableFuture1 = CompletableFuture.supplyAsync(getSupplier(future1));
+        CompletableFuture<ResponseEntity<Response>> uCompletableFuture2 = CompletableFuture.supplyAsync(getSupplier(future2));
+
+        uCompletableFuture1.thenApply(
+                (responseEntity) -> {
+                    System.out.println("complete");
+                    return responseEntity.getBody();
+                }
+        );
+
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(uCompletableFuture1, uCompletableFuture2);
+    }
+
+    private <U> Supplier<ResponseEntity<U>> getSupplier(ListenableFuture<ResponseEntity<U>> future) {
+        return () -> {
+            try {
+                return future.get();
+            } catch (InterruptedException | ExecutionException e) {
+             throw new RuntimeException();
+            }
+        };
+    }
+
+
+    public static class Response {
+        private String content;
+
+        public Response(String content) {
+            this.content = content;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+    }
+
+}
