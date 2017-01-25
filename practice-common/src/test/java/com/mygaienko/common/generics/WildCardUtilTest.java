af47321622d1893e1ac90ae9a487ed399549f877
dmygaenko <dmygaenko@gmail.com>
2017-01-25 13:57:54 +0200

    add loader tests

--- practice-common/src/test/java/com/mygaienko/common/generics/WildCardUtilTest.java
+++ practice-common/src/test/java/com/mygaienko/common/generics/WildCardUtilTest.java
@@ -3,6 +3,7 @@ package com.mygaienko.common.generics;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
@@ -10,7 +11,6 @@ import static org.junit.Assert.assertTrue;
  * Created by dmygaenko on 25/01/2017.
  */
 public class WildCardUtilTest {
-
 
     @Test
     public void testConsumer() throws Exception {
@@ -28,5 +28,14 @@ public class WildCardUtilTest {
     @Test
     public void testProducerExtends() throws Exception {
         WildCardUtil.producerExtends(new ArrayList<Integer>());
+    }
+
+    @Test
+    public void testCovarianceContravariance() throws Exception {
+        //List<Number> list = new ArrayList<Integer>(); //error covariance impossible
+        //List<Integer> list = new ArrayList<Number>(); //error contravariance impossible
+
+        List<? extends Number> producer = new ArrayList<Integer>(); //джокер символ
+        List<? super Integer> consumer = new ArrayList<Integer>();
     }
 }
