7dc6e5131858c9bf03571dee2d86ed82b37a6217
dmygaien <dmygaenko@gmail.com>
2017-01-26 01:12:08 +0200

    New changelist

--- practice-common/src/main/java/com/mygaienko/common/algorithms/e_olimp/ex3/Main.java
+++ practice-common/src/main/java/com/mygaienko/common/algorithms/e_olimp/ex3/Main.java
@@ -1,5 +1,8 @@
 package com.mygaienko.common.algorithms.e_olimp.ex3;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  *
  12											 ( 1 )
@@ -15,4 +18,30 @@ package com.mygaienko.common.algorithms.e_olimp.ex3;
 
  */
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+
+        PrintWriter out = new PrintWriter(System.out);
+        out.println(execute(a));
+        out.flush();
+    }
+
+    public static int execute(int a) {
+        int[] xyz = new int[] {1, 1, 1};
+        return collect(xyz, a, 0, 0);
+    }
+
+    private static int collect(int[] xyz, int n, int current, int matches) {
+        if (current == n || n == 0) return matches;
+
+        if (++xyz[0] * ++xyz[1] * ++xyz[2] < n) {
+            collectGreaterCub(xyz);
+        } else {
+            increasePanels();
+        }
+        return collect(xyz, n, current, matches);
+    }
+
 }
