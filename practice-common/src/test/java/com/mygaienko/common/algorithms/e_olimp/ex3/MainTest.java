a956df685e524227f7a1280b9f3ff14c04dcdad5
dmygaenko <dmygaenko@gmail.com>
2017-01-26 12:20:32 +0200

    add loader tests

--- practice-common/src/test/java/com/mygaienko/common/algorithms/e_olimp/ex3/MainTest.java
+++ practice-common/src/test/java/com/mygaienko/common/algorithms/e_olimp/ex3/MainTest.java
@@ -39,7 +39,8 @@ public class MainTest {
     public void testExecute() throws Exception {
         assertEquals(0, Main.execute(0));
         assertEquals(12, Main.execute(1));
-        assertEquals(12, Main.execute(8));
+        assertEquals(53, Main.execute(8));
+        assertEquals(136, Main.execute(27));
     }
 
     @Test
