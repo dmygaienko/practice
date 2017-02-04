a69fa3f2eb4ea8328e3f3ff2f177591dfeece521
dmygaien <dmygaenko@gmail.com>
2017-02-04 19:19:20 +0200

    New changelist

--- practice-common/src/test/java/com/mygaienko/common/collection/CollectionTest.java
+++ practice-common/src/test/java/com/mygaienko/common/collection/CollectionTest.java
@@ -2,16 +2,13 @@ package com.mygaienko.common.collection;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static com.mygaienko.common.util.TestUtils.getArrayList;
+import static com.mygaienko.common.util.TestUtils.getHashMap;
+import static com.mygaienko.common.util.TestUtils.getTreeMap;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -72,5 +69,66 @@ public class CollectionTest {
         map1.put("key4", null);
         map1.put("key3", "string3");
     }
+
+    @Test
+    public void testGetArrayList() {
+        assertEquals(Arrays.asList("1", "2", "3"), getArrayList(1, 3));
+    }
+
+    @Test
+    public void subList() {
+        List<String> strings = getArrayList(1, 10).subList(0, 5);
+        strings.set(0, "00");
+        System.out.println(strings);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void modifyArrayAsList() {
+        List<String> strings = Arrays.asList("1", "2", "3");
+        strings.add("4");
+        System.out.println(strings);
+    }
+
+    @Test
+    public void mergeMaps() {
+        TreeMap<String, String> m1 = getTreeMap(0, 5);
+        TreeMap<String, String> m2 = getTreeMap(4, 7);
+
+        TreeMap<String, String> merged = Stream.of(m1, m2)
+                .map(Map::entrySet)          // converts each map into an entry set
+                .flatMap(Collection::stream) // converts each set into an entry stream, then
+                // "concatenates" it in place of the original set
+                .collect(
+                        Collectors.toMap(        // collects into a map
+                                Map.Entry::getKey,   // where each entry is based
+                                Map.Entry::getValue, // on the entries in the stream
+                                (v1, v2) -> v1 + v2,
+                                TreeMap::new
+                        ));
+        System.out.println(merged);
+    }
+
+    @Test
+    public void testGetTreeMap() {
+        TreeMap<String, String> treeMap = getTreeMap(0, 5);
+        System.out.println(treeMap);
+    }
+
+    @Test
+    public void testGetHashMapAndCompute() {
+        HashMap<String, String> hashMap = getHashMap(0, 5);
+        hashMap.compute("6", (key, value) -> value == null ? "default" : value + value);
+        System.out.println(hashMap);
+    }
+
+    @Test
+    public void testGetHashMapAndComputeIfPresentOrAbasent() {
+        HashMap<String, String> hashMap = getHashMap(0, 5);
+        hashMap.computeIfPresent("1", (key, value) -> value + value);
+        hashMap.computeIfAbsent("6", (key) -> "default");
+        System.out.println(hashMap);
+    }
+
+
 
 }
