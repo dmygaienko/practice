0e0c90b5054c8a20a88a05a12f21996ae561ec32
dmygaien <dmygaenko@gmail.com>
2017-02-04 15:11:14 +0200

    New changelist

--- practice-common/src/test/java/com/mygaienko/common/algorithms/e_olimp/ex002/FileAnalyzerTest.java
+++ practice-common/src/test/java/com/mygaienko/common/algorithms/e_olimp/ex002/FileAnalyzerTest.java
@@ -1,5 +1,6 @@
 package com.mygaienko.common.algorithms.e_olimp.ex002;
 
+import antlr.collections.impl.IntRange;
 import org.h2.util.StringUtils;
 import org.junit.Test;
 
@@ -8,7 +9,13 @@ import java.math.BigDecimal;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Map;
 import java.util.Random;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 
 /**
@@ -41,6 +48,56 @@ public class FileAnalyzerTest {
     @Test
     public void testCurrentPath() throws IOException {
         System.out.println(Paths.get("").toAbsolutePath());
+    }
+
+    @Test
+    public void testSubMap() throws IOException {
+        TreeMap<String, String> map = new TreeMap<>();
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("3", "3");
+      /*  map.put("4", "4");*/
+        map.put("5", "5");
+        map.put("6", "6");
+        map.put("7", "7");
+        map.put("8", "8");
+
+        System.out.println(map.subMap("0", "4"));
+        System.out.println(map.headMap("4"));
+        System.out.println(map.tailMap("4"));
+        System.out.println(map);
+    }
+
+
+    @Test
+    public void mergeMaps() {
+        TreeMap<String, String> m1 = getTreeMap(0, 5);
+        TreeMap<String, String> m2 = getTreeMap(4, 7);
+
+        TreeMap<String, String> merged = Stream.of(m1, m2)
+                .map(Map::entrySet)          // converts each map into an entry set
+                .flatMap(Collection::stream) // converts each set into an entry stream, then
+                // "concatenates" it in place of the original set
+                .collect(
+                        Collectors.toMap(        // collects into a map
+                                Map.Entry::getKey,   // where each entry is based
+                                Map.Entry::getValue, // on the entries in the stream
+                                (v1, v2) -> v1 + v2,
+                                TreeMap::new
+                        ));
+        System.out.println(merged);
+    }
+
+    @Test
+    public void testGetTreeMap() {
+        System.out.println(getTreeMap(0, 5));
+    }
+
+    private TreeMap<String, String> getTreeMap(int i, int i1) {
+        return IntStream
+                .range(i, i1)
+                .boxed()
+                .collect(Collectors.toMap(String::valueOf, String::valueOf, (v1, v2) -> v1 + v2, TreeMap::new));
     }
 
 }
