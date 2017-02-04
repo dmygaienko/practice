8aa125252e894718c0d9da6f8c239ac3799d7b59
dmygaien <dmygaenko@gmail.com>
2017-02-04 19:19:20 +0200

    New changelist

--- practice-common/src/test/java/com/mygaienko/common/util/TestUtils.java
+++ practice-common/src/test/java/com/mygaienko/common/util/TestUtils.java
@@ -1,8 +1,11 @@
 package com.mygaienko.common.util;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toCollection;
 
 /**
  * Created by dmygaenko on 29/12/2016.
@@ -30,5 +33,33 @@ public class TestUtils {
             data.add(random.nextInt(bound));
         }
         return data;
+    }
+
+    public static TreeMap<String, String> getTreeMap(int i, int i1) {
+        return getMap(i, i1, TreeMap::new);
+    }
+
+    public static HashMap<String, String> getHashMap(int i, int i1) {
+        return getMap(i, i1, HashMap::new);
+    }
+
+    public static LinkedHashMap<String, String> getLinkedHashMap(int i, int i1) {
+        return getMap(i, i1, LinkedHashMap::new);
+    }
+
+
+    private static <T extends Map<String, String>> T getMap(int i, int i1, Supplier<T> mapSupplier) {
+        return IntStream
+                .range(i, ++i1)
+                .boxed()
+                .collect(Collectors.toMap(String::valueOf, String::valueOf, (v1, v2) -> v1 + v2, mapSupplier));
+    }
+
+    public static ArrayList<String> getArrayList(int i, int i1) {
+        return IntStream
+                .range(i, ++i1)
+                .boxed()
+                .map(String::valueOf)
+                .collect(toCollection(ArrayList::new));
     }
 }
