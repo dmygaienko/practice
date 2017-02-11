e775608cbaf510cc42f79e7c3e995bf21c185f0f
dmygaien <dmygaenko@gmail.com>
2017-02-11 21:59:35 +0200

    ex431

--- practice-common/src/main/java/com/mygaienko/common/algorithms/e_olimp/ex431/Main.java
+++ practice-common/src/main/java/com/mygaienko/common/algorithms/e_olimp/ex431/Main.java
@@ -1,6 +1,7 @@
 package com.mygaienko.common.algorithms.e_olimp.ex431;
 
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
@@ -27,16 +28,35 @@ public class Main {
                 tree[u][v] = 1;
             });
 
-            out.println(countSets(tree, weights, k));
+            out.println(countSets(tree, weights, n, k));
             out.flush();
         });
     }
 
-    private static long countSets(int[][] tree, int[] weights, int k) {
-        for (int i = 0; i < 1; i++) {
+    private static long countSets(int[][] tree, int[] weights, int n, int k) {
+        long sets = 0;
+
+        //check all vertices as possible root for set
+        for (int i = 1; i <= n; i++) {
+
+            int sum = weights[i];
+            if (sum == k) {
+                sets += 1;
+            }
+
+            for (int v = 1; v <= n ; v++) {
+                int nextV = tree[i][v];
+
+                if (v == 1) {
+
+                }
+            }
+
+
 
         }
-        return 0;
+
+        return sets;
     }
 
 }
