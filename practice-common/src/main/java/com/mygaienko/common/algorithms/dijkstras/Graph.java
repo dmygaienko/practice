e1dc5b91e5d5658498a7026c4128e941cf2da489
dmygaien <dmygaenko@gmail.com>
2017-02-13 00:51:50 +0200

    ex431

---
+++ practice-common/src/main/java/com/mygaienko/common/algorithms/dijkstras/Graph.java
@@ -0,0 +1,25 @@
+package com.mygaienko.common.algorithms.dijkstras;
+
+/**
+ * Created by enda1n on 13.02.2017.
+ */
+import java.util.List;
+
+public class Graph {
+    private final List<Vertex> vertexes;
+    private final List<Edge> edges;
+
+    public Graph(List<Vertex> vertexes, List<Edge> edges) {
+        this.vertexes = vertexes;
+        this.edges = edges;
+    }
+
+    public List<Vertex> getVertexes() {
+        return vertexes;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+}
