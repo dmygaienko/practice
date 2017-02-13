c0e84937ecb5d96321927a785c7cf49307f45c41
dmygaien <dmygaenko@gmail.com>
2017-02-14 01:10:47 +0200

    ex431

--- practice-common/src/test/java/com/mygaienko/common/algorithms/e_olimp/ex431/MainTest.java
+++ practice-common/src/test/java/com/mygaienko/common/algorithms/e_olimp/ex431/MainTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class MainTest {
 
     @Test
-    public void name() throws Exception {
+    public void testSimpleTree() throws Exception {
         List<List<Integer>> tree = new ArrayList<>();
         tree.add(0, Collections.emptyList());
         tree.add(1, Arrays.asList(2, 3, 4));
@@ -26,6 +26,28 @@ public class MainTest {
         paths.add(2, new ArrayList<>());
         paths.add(3, new ArrayList<>());
         paths.add(4, new ArrayList<>());
+        Main.countPathsInSubtree(tree, 1, paths);
+
+    }
+
+    @Test
+    public void testSimpleTreeWithSubtree() throws Exception {
+        List<List<Integer>> tree = new ArrayList<>();
+        tree.add(0, Collections.emptyList());
+        tree.add(1, Arrays.asList(2, 3, 4));
+        tree.add(2, Collections.emptyList());
+        tree.add(3, Collections.emptyList());
+        tree.add(4, Arrays.asList(5, 6));
+        tree.add(5, Collections.emptyList());
+        tree.add(6, Collections.emptyList());
+        List<List<List<Integer>>> paths = new ArrayList<>();
+        paths.add(0, Collections.emptyList());
+        paths.add(1, new ArrayList<>());
+        paths.add(2, new ArrayList<>());
+        paths.add(3, new ArrayList<>());
+        paths.add(4, new ArrayList<>());
+        paths.add(5, new ArrayList<>());
+        paths.add(6, new ArrayList<>());
         Main.countPathsInSubtree(tree, 1, paths);
 
     }
