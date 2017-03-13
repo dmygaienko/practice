ffc72b81b6d18d1ff2906a2945617296d4548813
dmygaien <dmygaenko@gmail.com>
2017-03-13 23:49:40 +0200

    ex431

--- practice-common/src/main/java/com/mygaienko/common/algorithms/condingame/teads-sponsored-contest.js
+++ practice-common/src/main/java/com/mygaienko/common/algorithms/condingame/teads-sponsored-contest.js
@@ -17,6 +17,10 @@ for (var i = 0; i < n; i++) {
 }
 console.log(JSON.stringify(network));
 
+network.countBroadcast();
+
+console.log(network.findMinBroadcast());
+
 // Write an action using print()
 // To debug: printErr('Debug messages...');
 
@@ -38,6 +42,107 @@ function Network() {
             this.content[b] = new Person(b);
         }
         this.content[b].addInverseAdjacent(a);
+    };
+
+    this.countBroadcast = function() {
+        this.countAllDirectBroadcast();
+        this.countAllInverseBroadcast();
+    };
+
+    this.countAllDirectBroadcast = function() {
+        this.countDirectBroadcast(0);
+    };
+
+    this.countDirectBroadcast = function(index) {
+
+        var currentPerson = this.content[index];
+        var directAdjacent = currentPerson.directAdjacent;
+
+        var directBroadcast;
+
+        if (currentPerson.directBroadcast !== undefined) {
+            directBroadcast = currentPerson.directBroadcast;
+        } else {
+
+            directBroadcast = 0;
+            for (var i = 0; i < directAdjacent.length; i++) {
+                var currentBroadcast = this.countDirectBroadcast(directAdjacent[i]);
+                if (directBroadcast <= currentBroadcast) {
+                    directBroadcast = currentBroadcast;
+                }
+            }
+            currentPerson.directBroadcast = directBroadcast;
+        }
+        return directBroadcast + 1;
+    };
+
+    this.countAllInverseBroadcast = function() {
+
+        for (var i = 0; i < this.content.length; i++) {
+            var person = this.content[i];
+            if (person !== undefined) {
+                this.countInverseBroadcast(person.index);
+            }
+        }
+
+    };
+
+    this.countInverseBroadcast = function(index) {
+        var currentPerson = this.content[index];
+        var inverseAdjacent = currentPerson.inverseAdjacent;
+
+        var inverseBroadcast;
+
+        if (currentPerson.inverseBroadcast !== undefined) {
+            inverseBroadcast = currentPerson.inverseBroadcast;
+        } else {
+            inverseBroadcast = 0;
+            //max beetween
+            //inverse broadcast of parent node
+            //vs direct broadcast of parent node but another connection
+            //+1
+            for (var i = 0; i < inverseAdjacent.length; i++) {
+                var parent = this.content[inverseAdjacent[i]];
+                inverseBroadcast = this.countInverseBroadcast(parent.index);
+
+                var children = parent.directAdjacent.filter(function(id){
+                    return id != index;
+                })
+
+                for (var j = 0; j < children.length; j++) {
+                    var child = this.content[children[i]];
+                    var toChildrenDirectBroadcast = child.directBroadcast + 1
+
+                    if (inverseBroadcast <= toChildrenDirectBroadcast) {
+                        inverseBroadcast = toChildrenDirectBroadcast;
+                    }
+                }
+            }
+
+            if (inverseBroadcast > 0) {
+                inverseBroadcast++;
+            }
+            currentPerson.inverseBroadcast = inverseBroadcast;
+
+        }
+        return inverseBroadcast;
+
+    };
+
+    this.findMinBroadcast = function() {
+        var minBroadcastIndex;
+        var minBroadcast = Number.MAX_VALUE;
+        for (var i = 0; i < this.content.length; i++) {
+            var person = this.content[i];
+            var broadcast = person.directBroadcast + person.inverseBroadcast;
+
+            if (broadcast <= minBroadcast) {
+                minBroadcast = broadcast;
+                minBroadcastIndex = person.index;
+            }
+        }
+
+        return minBroadcastIndex;
     }
 }
 
@@ -45,6 +150,9 @@ function Person(index, b) {
     this.index = index;
     this.directAdjacent = [];
     this.inverseAdjacent = [];
+
+    this.directBroadcast = undefined;
+    this.inverseBroadcast = undefined;
 
     this.addDirectAdjacent = function(b) {
         this.directAdjacent.push(b);
@@ -57,5 +165,4 @@ function Person(index, b) {
     if (b !== undefined) {
         this.addDirectAdjacent(b);
     }
-
 }
