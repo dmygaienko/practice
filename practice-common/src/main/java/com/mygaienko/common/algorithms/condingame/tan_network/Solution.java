0564cd91ddc42895a5d911ff91ca450fbb5779a7
dmygaien <dmygaenko@gmail.com>
2017-03-25 21:42:57 +0200

    codingame tan_network

--- practice-common/src/main/java/com/mygaienko/common/algorithms/condingame/tan_network/Solution.java
+++ practice-common/src/main/java/com/mygaienko/common/algorithms/condingame/tan_network/Solution.java
@@ -1,8 +1,11 @@
 package com.mygaienko.common.algorithms.condingame.tan_network;
 
 import java.util.*;
-import java.io.*;
-import java.math.*;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.cos;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -10,20 +13,115 @@ import java.math.*;
  **/
 class Solution {
 
+    private static List<String> rawStops = new ArrayList<>();
+    private static List<String> rawRoutes = new ArrayList<>();
+
     private static Map<String, StopArea> stops = new HashMap<>();
 
     public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        String startPoint = in.next();
-        String endPoint = in.next();
+       /* Scanner in = new Scanner(System.in);
+        String startStopId = in.next();
+        String endStopId = in.next();*/
 
-        readStops(in);
-        readRoutes(in);
+        String startStopId = "StopArea:ABDU";
+        String endStopId = "StopArea:ABLA";
 
+        TanUtil.initRaw(rawStops, rawRoutes);
+        /*readStops(in);
+        readRoutes(in);*/
+        parseRaw();
+
+        fillTargetDistances(endStopId);
+
+        List<StopArea> path = aStar(stops.get(startStopId), stops.get(endStopId));
         // Write an action using System.out.println()
         // To debug: System.err.println("Debug messages...");
 
-        System.out.println("IMPOSSIBLE");
+        printPath(path);
+    }
+
+    private static void printPath(List<StopArea> path) {
+        System.out.println(path.isEmpty() ? "IMPOSSIBLE" :
+                path.stream()
+                        .map(stop -> stop.getDescription().replaceAll("\"", ""))
+                        .collect(Collectors.joining("\n")));
+    }
+
+    public static List<StopArea> aStar(StopArea startStopArea, StopArea targetStopArea) {
+        Set<StopArea> openQueue = new HashSet<>();
+        Set<StopArea> closedQueue = new HashSet<>();
+
+        Map<StopArea, StopArea> previous = new HashMap<>();
+        Map<StopArea, Double> gScore = new HashMap<>();
+        Map<StopArea, Double> fScore = new HashMap<>();
+
+        openQueue.add(startStopArea);
+        gScore.put(startStopArea, 0d);
+
+        while (!openQueue.isEmpty()) {
+            StopArea currentStopArea = findSmallestStopArea(openQueue, fScore);
+
+            if (currentStopArea.equals(targetStopArea)) {
+                return reconstructPath(currentStopArea, previous);
+            }
+
+            openQueue.remove(currentStopArea);
+            closedQueue.add(currentStopArea);
+
+            for (StopArea nextStopArea : currentStopArea.nextStops) {
+                if (closedQueue.contains(nextStopArea)) {
+                    continue; //ignore  the neighbour which is already evaluated
+                }
+
+                Double tentativeGScore = gScore.get(currentStopArea) + countDistance(currentStopArea, nextStopArea);
+                if (!openQueue.add(nextStopArea)) {
+                    if (tentativeGScore >= gScore.get(nextStopArea)) {
+                        continue; // this is not the best path
+                    }
+                }
+
+                //this path is best until now
+                previous.put(nextStopArea, currentStopArea);
+                gScore.put(nextStopArea, tentativeGScore);
+                fScore.put(nextStopArea, tentativeGScore + nextStopArea.getTargetDistance());
+            }
+        }
+
+        return Collections.EMPTY_LIST;
+    }
+
+    private static List<StopArea> reconstructPath(StopArea current, Map<StopArea, StopArea> previouses) {
+        List<StopArea> path = new ArrayList<>();
+
+        while (current != null) {
+            path.add(current);
+            current = previouses.get(current);
+        }
+
+        Collections.reverse(path);
+        return path;
+    }
+
+    private static StopArea findSmallestStopArea(Set<StopArea> open, Map<StopArea, Double> fScores) {
+        return open.stream()
+                .reduce((a, b) -> fScores.get(a) < fScores.get(b) ? a : b)
+                .get();
+    }
+
+    private static void parseRaw() {
+        rawStops.forEach(Solution::parseStop);
+        rawRoutes.forEach(Solution::parseRoute);
+    }
+
+    private static void fillTargetDistances(String targetStopId) {
+        StopArea targetStop = stops.get(targetStopId);
+        stops.values().forEach(stop -> stop.setTargetDistance(countDistance(stop, targetStop)));
+    }
+
+    private static double countDistance(StopArea a, StopArea b) {
+        double x = (b.longitude - a.longitude) * cos((a.latitude + b.latitude)/2);
+        double y = (b.latitude - a.latitude);
+        return sqrt(pow(x, 2) + pow(y, 2)) * 6371;
     }
 
     private static void readRoutes(Scanner in) {
@@ -32,12 +130,12 @@ class Solution {
             in.nextLine();
         }
         for (int i = 0; i < M; i++) {
-            parseRoute(in.nextLine());
+            rawRoutes.add(in.nextLine());
         }
     }
 
     private static void parseRoute(String route) {
-        String[] ids = route.split(",");
+        String[] ids = route.split(" ");
         String startStopId = ids[0];
         String endStopId = ids[1];
 
@@ -52,7 +150,7 @@ class Solution {
             in.nextLine();
         }
         for (int i = 0; i < N; i++) {
-            parseStop(in.nextLine());
+            rawStops.add(in.nextLine());
         }
     }
 
@@ -61,65 +159,76 @@ class Solution {
         StopArea stopArea = new StopArea(
                 attributes[0], attributes[1], attributes[2],
                 attributes[3], attributes[4], attributes[5],
-                attributes[6], attributes[7], attributes[8]
+                attributes[6], attributes[7]
         );
-        stops.put(stopArea.getId(), stopArea);
+        stops.put(stopArea.id, stopArea);
     }
 
-    private static class StopArea {
+    static class StopArea {
 
         private final String id;
         private final String fullName;
         private final String description;
-        private final String latitude;
-        private final String longitude;
+        private final double latitude;
+        private final double longitude;
         private final String zoneId;
         private final String url;
         private final String type;
-        private final String motherStation;
 
-        private Set<StopArea> nextStops = new HashSet<>();
+        private final Set<StopArea> nextStops = new HashSet<>();
+        private final Map<StopArea, Double> nextStopsDistances = new HashMap<>();
 
-        public StopArea(String id, String fullName, String description,
-                        String latitude, String longitude, String zoneId,
-                        String url, String type, String motherStation) {
+        private double targetDistance;
+
+        StopArea(String id, String description, String fullName,
+                 String latitude, String longitude, String zoneId,
+                 String url, String type) {
             this.id = id;
-            this.fullName = fullName;
             this.description = description;
-            this.latitude = latitude;
-            this.longitude = longitude;
+            this.fullName = fullName;
+            this.latitude = Double.parseDouble(latitude);
+            this.longitude = Double.parseDouble(longitude);
             this.zoneId = zoneId;
             this.url = url;
             this.type = type;
-            this.motherStation = motherStation;
         }
 
-        public String getId() {
-            return id;
-        }
-
-        public String getFullName() {
-            return fullName;
-        }
-
-        public String getLatitude() {
-            return latitude;
-        }
-
-        public String getLongitude() {
-            return longitude;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public void addRoute(StopArea nextStop) {
+        void addRoute(StopArea nextStop) {
             nextStops.add(nextStop);
+            nextStopsDistances.put(this, countDistance(this, nextStop));
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        double getDistanceTo(StopArea nextStop) {
+            return nextStopsDistances.get(nextStop);
         }
 
         public Set<StopArea> getNextStops() {
             return nextStops;
+        }
+
+        public double getTargetDistance() {
+            return targetDistance;
+        }
+
+        public void setTargetDistance(double targetDistance) {
+            this.targetDistance = targetDistance;
+        }
+
+        @Override
+        public String toString() {
+            return "id='" + id + '\'' +
+                    ", lat=" + latitude +
+                    ", long=" + longitude +
+                    ", dist=" + targetDistance +
+                    ", nextSt=" + nextStops.stream().reduce(
+                            new StringBuilder(),
+                            (builder, stopArea) -> builder.append(stopArea.id),
+                            (a, b) -> a.append(b.toString())).toString() +
+                    '}';
         }
 
         @Override
@@ -137,5 +246,6 @@ class Solution {
         public int hashCode() {
             return id != null ? id.hashCode() : 0;
         }
+
     }
 }
