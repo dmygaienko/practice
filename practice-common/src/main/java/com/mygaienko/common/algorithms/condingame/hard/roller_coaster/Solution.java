61c27505e7b8a3c74bae93abfeeb413f50596b99
dmygaien <dmygaenko@gmail.com>
2017-03-31 01:13:39 +0300

    codingame tan_network

--- practice-common/src/main/java/com/mygaienko/common/algorithms/condingame/hard/roller_coaster/Solution.java
+++ practice-common/src/main/java/com/mygaienko/common/algorithms/condingame/hard/roller_coaster/Solution.java
@@ -6,6 +6,9 @@ import java.util.*;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+
+//ArrayDeque
+// delete by scope with index
 class Solution {
 
     private static Map<Integer, RidedGroup> interimResults = new HashMap<>();
