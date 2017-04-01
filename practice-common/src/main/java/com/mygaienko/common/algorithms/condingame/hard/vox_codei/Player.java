49b4ca906ee827fb7baa82313fefef5fbd2e745a
dmygaien <dmygaenko@gmail.com>
2017-04-01 23:30:17 +0300

    codingame tan_network

--- practice-common/src/main/java/com/mygaienko/common/algorithms/condingame/hard/vox_codei/Player.java
+++ practice-common/src/main/java/com/mygaienko/common/algorithms/condingame/hard/vox_codei/Player.java
@@ -11,15 +11,14 @@ import static java.util.stream.Collectors.toList;
  **/
 class Player {
 
-    private static List<List<Node>> grid;
-    private static Set<Node> survNodes = new HashSet<>();
-    private static Map<Integer, Set<Node>> toExplode = new HashMap<>();
     private static int SIDE_EXPLOSION = 3;
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
 
-        initGrid(in);
+        Set<Node> survNodes = new HashSet<>();
+        List<List<Node>> grid = initGrid(in, survNodes);
+        Map<Integer, Set<Node>> toExplode = new HashMap<>();
 
         // game loop
 
@@ -27,22 +26,22 @@ class Player {
             int rounds = in.nextInt(); // number of rounds left before the end of the game
             int bombs = in.nextInt(); // number of bombs left
 
-            processExplosion(round);
+            processExplosion(toExplode, round);
 
             String result = "WAIT";
             BombLocationResult bombResult;
             if (bombs > 0 && !survNodes.isEmpty()) {
-                bombResult = getMostProductiveBombLocation();
+                bombResult = getMostProductiveBombLocation(grid);
                 if (NodeType.EMPTY.equals(bombResult.location.type)) {
                     result = bombResult.location.width + " " + bombResult.location.height;
-                    mineNodes(bombResult.exploded, round + 3);
+                    mineNodes(survNodes, toExplode, bombResult.exploded, round + 3);
                 }
             }
             System.out.println(result);
         }
     }
 
-    private static void processExplosion(int round) {
+    private static void processExplosion(Map<Integer, Set<Node>> toExplode, int round) {
         Set<Node> nodes = toExplode.get(round);
         if (nodes != null) {
             for (Node node : nodes) {
@@ -51,7 +50,7 @@ class Player {
         }
     }
 
-    private static void mineNodes(Set<Node> exploded, int round) {
+    private static void mineNodes(Set<Node> survNodes, Map<Integer, Set<Node>> toExplode, Set<Node> exploded, int round) {
         for (Node node : exploded) {
             node.type = NodeType.MINED;
         }
@@ -59,58 +58,71 @@ class Player {
         toExplode.put(round, exploded);
     }
 
-    private static BombLocationResult getMostProductiveBombLocation() {
+   /* private static BombLocationResult getMostProductiveBombLocation(List<List<Node>> grid) {
+        //List<Node> bombLocations = getNearestBombLocations(next);
+        List<BombLocationResult> steps = grid.stream()
+                .flatMap(line -> line.stream())
+                .map(bombLocation -> explode(bombLocation, grid))
+                .sorted((a, b) -> a.result() < b.result() ? 1 : (a.result() > b.result()) ? -1 : 0)
+                .collect(toList());
+
+        List<BombLocationResult> successSteps = defineStepsToExplodeAll(steps);
+        return successSteps.get(0);
+    }*/
+
+
+    private static BombLocationResult getMostProductiveBombLocation(List<List<Node>> grid) {
         //List<Node> bombLocations = getNearestBombLocations(next);
         return grid.stream()
                 .flatMap(line -> line.stream())
-                .map(bombLocation -> explode(bombLocation))
+                .map(bombLocation -> explode(bombLocation, grid))
                 .reduce((r1, r2) -> r2.result() > r1.result() ? r2 : r1)
                 .get();
     }
 
-    private static BombLocationResult explode(Node bombLocation) {
+    private static BombLocationResult explode(Node bombLocation, List<List<Node>> grid) {
         BombLocationResult result = new BombLocationResult(bombLocation);
         NodeType type = bombLocation.type;
         if (NodeType.EMPTY.equals(type) || NodeType.MINED.equals(type)) {
-            result.exploded.addAll(explodeUp(bombLocation));
-            result.exploded.addAll(explodeDown(bombLocation));
-            result.exploded.addAll(explodeRight(bombLocation));
-            result.exploded.addAll(explodeLeft(bombLocation));
+            result.exploded.addAll(explodeUp(bombLocation, grid));
+            result.exploded.addAll(explodeDown(bombLocation, grid));
+            result.exploded.addAll(explodeRight(bombLocation, grid));
+            result.exploded.addAll(explodeLeft(bombLocation, grid));
         }
         return result;
     }
 
-    private static List<Node> explodeUp(Node bombLocation) {
+    private static List<Node> explodeUp(Node bombLocation, List<List<Node>> grid) {
         ArrayList<Node> result = new ArrayList<>();
         for (int i = bombLocation.height - 1, l = 0; i > -1 && l < SIDE_EXPLOSION; i--, l++) {
-            if (isNotExplosiveNode(result, i, bombLocation.width)) break;
+            if (isNotExplosiveNode(grid, result, i, bombLocation.width)) break;
         }
         return result;
     }
-    private static List<Node> explodeDown(Node bombLocation) {
+    private static List<Node> explodeDown(Node bombLocation, List<List<Node>> grid) {
         ArrayList<Node> result = new ArrayList<>();
         for (int i = bombLocation.height + 1, l = 0; i < grid.size() && l < SIDE_EXPLOSION; i++, l++) {
-            if (isNotExplosiveNode(result, i, bombLocation.width)) break;
+            if (isNotExplosiveNode(grid, result, i, bombLocation.width)) break;
         }
         return result;
     }
-    private static List<Node> explodeRight(Node bombLocation) {
+    private static List<Node> explodeRight(Node bombLocation, List<List<Node>> grid) {
         ArrayList<Node> result = new ArrayList<>();
         List<Node> line = grid.get(bombLocation.height);
         for (int i = bombLocation.width + 1, l = 0; i < line.size() && l < SIDE_EXPLOSION; i++, l++) {
-            if (isNotExplosiveNode(result, bombLocation.height, i)) break;
+            if (isNotExplosiveNode(grid, result, bombLocation.height, i)) break;
         }
         return result;
     }
-    private static List<Node> explodeLeft(Node bombLocation) {
+    private static List<Node> explodeLeft(Node bombLocation, List<List<Node>> grid) {
         ArrayList<Node> result = new ArrayList<>();
         for (int i = bombLocation.width - 1, l = 0; i > -1 && l < SIDE_EXPLOSION; i--, l++) {
-            if (isNotExplosiveNode(result, bombLocation.height, i)) break;
+            if (isNotExplosiveNode(grid, result, bombLocation.height, i)) break;
         }
         return result;
     }
 
-    private static boolean isNotExplosiveNode(ArrayList<Node> result, int height, int width) {
+    private static boolean isNotExplosiveNode(List<List<Node>> grid, ArrayList<Node> result, int height, int width) {
         Node node = grid.get(height).get(width);
         if (NodeType.PASSIVE.equals(node.type)){
             return true;
@@ -120,75 +132,26 @@ class Player {
         return false;
     }
 
-    private static List<Node> getNearestBombLocations(Node next) {
-        ArrayList<Node> neighbours = new ArrayList<>();
-        neighbours.addAll(getUpNeighbours(next));
-        neighbours.addAll(getDownNeighbours(next));
-        neighbours.addAll(getLeftNeighbours(next));
-        neighbours.addAll(getRightNeighbours(next));
-        return neighbours;
-    }
-
-    private static boolean isNotBombLocation(ArrayList<Node> result, int height, int width) {
-        Node neighbour = grid.get(height).get(width);
-        NodeType neighbourType = neighbour.type;
-        if (NodeType.PASSIVE.equals(neighbourType)) {
-            return true;
-        } else if (NodeType.EMPTY.equals(neighbourType)) {
-            result.add(neighbour);
-        }
-        return false;
-    }
-
-    private static List<Node> getUpNeighbours(Node node) {
-        ArrayList<Node> result = new ArrayList<>();
-        for (int i = node.height - 1, l = 0; i > -1 && l < SIDE_EXPLOSION; i--, l++) {
-            if (isNotBombLocation(result, i, node.width)) break;
-        }
-        return result;
-    }
-
-    private static List<Node> getDownNeighbours(Node node) {
-        ArrayList<Node> result = new ArrayList<>();
-        for (int i = node.height + 1, l = 0; i < grid.size() && l < SIDE_EXPLOSION; i++, l++) {
-            if (isNotBombLocation(result, i, node.width)) break;
-        }
-        return result;
-    }
-    private static List<Node> getLeftNeighbours(Node node) {
-        ArrayList<Node> result = new ArrayList<>();
-        for (int i = node.width - 1, l = 0; i > -1 && l < SIDE_EXPLOSION; i--, l++) {
-            if (isNotBombLocation(result, node.height, i)) break;
-        }
-        return result;
-    }
-    private static List<Node> getRightNeighbours(Node node) {
-        ArrayList<Node> result = new ArrayList<>();
-        List<Node> line = grid.get(node.height);
-        for (int i = node.width + 1, l = 0; i < line.size() && l < SIDE_EXPLOSION; i++, l++) {
-            if (isNotBombLocation(result, node.height, i)) break;
-        }
-        return result;
-    }
-
-    private static void initGrid(Scanner in) {
+    private static List<List<Node>> initGrid(Scanner in, Set<Node> survNodes) {
         int width = in.nextInt(); // width of the firewall grid
         int height = in.nextInt(); // height of the firewall grid
         if (in.hasNextLine()) {
             in.nextLine();
         }
 
-        grid = new ArrayList<>(height);
+        List<List<Node>> grid = new ArrayList<>(height);
 
         IntStream.range(0, height)
                 .forEach(h -> {
                     String[] splitted = in.nextLine().split("");
-                    List<Node> line = initLines(width, h, splitted);
+                    List<Node> line = initLines(width, h, splitted, survNodes);
                     grid.add(line);
                 });
+
+        return grid;
     }
 
-    private static List<Node> initLines(int width, int h, String[] splitted) {
+    private static List<Node> initLines(int width, int h, String[] splitted, Set<Node> survNodes) {
         return IntStream.range(0, width)
                                 .mapToObj(w -> {
                                     Node node = new Node(splitted[w], w, h);
