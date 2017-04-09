ab994cebb546504304d0a7a1cdd842abfa13880d
dmygaien <dmygaenko@gmail.com>
2017-04-10 00:45:37 +0300

    codingame tan_network

--- practice-common/src/main/java/com/mygaienko/common/algorithms/condingame/very_hard/mars-lander-episode-3.js
+++ practice-common/src/main/java/com/mygaienko/common/algorithms/condingame/very_hard/mars-lander-episode-3.js
@@ -1,3 +1,15 @@
+// def getNextState(lander : Lander, action : Action) : Lander = {
+//     var nextP = lander.power + action.power
+//     var nextFuel = lander.fuel - nextP
+//     var nextAngle = lander.angle + action.angle
+//     var nextX = 2 * lander.x - lander.prevX - nextP * sin(toRadians(nextAngle))
+//     var nextY = 2 * lander.y - lander.prevY - g + nextP * cos(toRadians(nextAngle))
+//     var nextVx = lander.hSpeed - nextP * sin(toRadians(nextAngle))
+//     var nextVy = lander.vSpeed - g + nextP * cos(toRadians(nextAngle))
+//     var nextLander = Lander(nextX, nextY, lander.x, lander.y, nextVx, nextVy, nextFuel, nextAngle, nextP)
+//     nextLander
+// }
+
 //var surfaces = ['0 450','300 750','1000 450','1500 650','1800 850','2000 1950','2200 1850','2400 2000','3100 1800','3150 1550','2500 1600','2200 1550','2100 750','2200 150','3200 150','3500 450','4000 950','4500 1450','5000 1550','5500 1500','6000 950','6999 1750'];
 var surfaces = ['0 1800', '300 1200', '1000 1550', '2000 1200', '2500 1650', '3700 220', '4700 220', '4750 1000', '4700 1650', '4000 1700', '3700 1600', '3750 1900', '4000 2100', '4900 2050', '5100 1000', '5500 500', '6200 800', '6999 600'];
 
