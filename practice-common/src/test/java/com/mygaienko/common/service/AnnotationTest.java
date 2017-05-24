9aefe280a133914b3a2aefcdee76e21b0728174e
dmygaenko <dmygaenko@gmail.com>
2017-05-24 14:15:19 +0300

    New changelist1

--- practice-common/src/test/java/com/mygaienko/common/service/AnnotationTest.java
+++ practice-common/src/test/java/com/mygaienko/common/service/AnnotationTest.java
@@ -2,11 +2,18 @@ package com.mygaienko.common.service;
 
 import com.mygaienko.common.annotations.MyAttributeAnnotation;
 import com.mygaienko.common.annotations.MyBeanAnnotation;
+import com.mygaienko.common.annotations.MyMethodAnnotation;
 import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.reflect.MethodUtils;
 import org.junit.Test;
 import org.springframework.core.annotation.AnnotationUtils;
 
 import java.lang.reflect.Field;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isIn;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by dmygaenko on 24/05/2017.
@@ -16,15 +23,24 @@ public class AnnotationTest {
     @Test
     public void testAnnotations() throws Exception {
         MyBeanAnnotation annotation = AnnotationUtils.findAnnotation(B.class, MyBeanAnnotation.class);
+        assertThat(annotation.customName(), is("beanCustomName"));
     }
 
     @Test
-    public void testDeclaringAnnotations() throws Exception {
+    public void testGetFieldsWithAnnotation() throws Exception {
         Field[] annotatedFields = FieldUtils.getFieldsWithAnnotation(B.class, MyAttributeAnnotation.class);
         for (Field annotatedField : annotatedFields) {
             MyAttributeAnnotation annotation = annotatedField.getAnnotation(MyAttributeAnnotation.class);
-            System.out.println(annotation.name());
+            assertThat(annotation.name(), isIn(asList("b1Annotation", "a1Annotation")));
         }
+    }
+
+    @Test
+    public void testGetMethodsListWithAnnotation() throws Exception {
+        MethodUtils.getMethodsListWithAnnotation(B.class, MyMethodAnnotation.class)
+                .stream()
+                .map(field -> field.getAnnotation(MyMethodAnnotation.class))
+                .forEach(annotation -> assertThat(annotation.methodName(), isIn(asList("methodA1Annotation", "methodB1Annotation"))));
     }
 
 }
@@ -35,6 +51,7 @@ class A {
     @MyAttributeAnnotation(name = "a1Annotation")
     private String a1;
 
+    @MyMethodAnnotation(methodName = "methodA1Annotation")
     public String getA1() {
         return a1;
     }
@@ -50,6 +67,7 @@ class B extends A {
     @MyAttributeAnnotation(name = "b1Annotation")
     private String b1;
 
+    @MyMethodAnnotation(methodName = "methodB1Annotation")
     public String getB1() {
         return b1;
     }
