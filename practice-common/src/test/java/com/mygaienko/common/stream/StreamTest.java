58bd06789995a53a6dda0f29bfe0ebd4d95a2249
dmygaenko <dmygaenko@gmail.com>
2017-05-31 14:59:29 +0300

    New changelist1

--- practice-common/src/test/java/com/mygaienko/common/stream/StreamTest.java
+++ practice-common/src/test/java/com/mygaienko/common/stream/StreamTest.java
@@ -710,7 +710,7 @@ public class StreamTest {
     @Test
     public void batchCollector() throws Exception {
         IntStream.range(0, 50)
-                .parallel()
+                //.parallel()
                 .boxed()
                 .collect(new BatchCollector<>(System.out::println, 5));
     }
