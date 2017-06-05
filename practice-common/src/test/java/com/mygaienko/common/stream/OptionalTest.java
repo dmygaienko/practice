e60559ac7c583e17802b69b1b8e2ac7a34118bd2
dmygaien <dmygaenko@gmail.com>
2017-06-05 09:51:02 +0300

    commit2.3.1.2

---
+++ practice-common/src/test/java/com/mygaienko/common/stream/OptionalTest.java
@@ -0,0 +1,30 @@
+package com.mygaienko.common.stream;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by enda1n on 05.06.2017.
+ */
+public class OptionalTest {
+
+    @Test
+    public void flatMapOptional() throws Exception {
+
+        final Optional<Optional<Object>> emptyOfEmpty = Optional.of(Optional.empty());
+        System.out.println("empty of empty: " + emptyOfEmpty);
+
+        final Optional<String> empty = emptyOfEmpty
+                //.filter(Optional::isPresent)
+                .flatMap(optional -> Optional.of(optional.orElse("") + "modified"));
+
+        assertThat(emptyOfEmpty, is(Optional.of(Optional.empty())));
+        assertThat(empty, is(Optional.of("modified")));
+
+        System.out.println(empty);
+    }
+}
