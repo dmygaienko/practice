1733593ea4b5d9ba6518d65f1f5236d066b231aa
dmygaien <dmygaenko@gmail.com>
2017-06-08 23:23:19 +0300

    commit2.3.1.2

--- practice-jpa/src/main/java/com/mygaienko/practice/jpa/model/CitiesWrapper.java
+++ practice-jpa/src/main/java/com/mygaienko/practice/jpa/model/CitiesWrapper.java
@@ -6,7 +6,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import java.io.Serializable;
-import java.util.List;
+import java.util.Set;
 
 /**
  * Created by liuda on 12/6/2015.
@@ -18,13 +18,13 @@ public class CitiesWrapper implements Serializable {
     // when remove from collection and persist city also will be deleted on database
     @JsonManagedReference
     @OneToMany(mappedBy = "country", orphanRemoval = true, cascade = CascadeType.PERSIST)
-    private List<City> cities;
+    private Set<City> cities;
 
-    public List<City> getCities() {
+    public Set<City> getCities() {
         return cities;
     }
 
-    public void setCities(List<City> cities) {
+    public void setCities(Set<City> cities) {
         this.cities = cities;
     }
 }
