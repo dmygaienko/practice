96b9db22bc24d6acc4a52db8734a0e366bd476d1
dmygaien <dmygaenko@gmail.com>
2017-06-08 23:20:11 +0300

    commit2.3.1.2

---
+++ practice-patterns/src/test/java/com/mygaienko/patterns/creational/factorymethod/FactoryServiceTest.java
@@ -0,0 +1,17 @@
+package com.mygaienko.patterns.creational.factorymethod;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by enda1n on 08.06.2017.
+ */
+public class FactoryServiceTest {
+    @Test
+    public void getService() throws Exception {
+        Service<Entity2> service = FactoryService.getService(ServiceEnum.ENUM1);
+        service.doService(new Entity2());
+    }
+
+}
