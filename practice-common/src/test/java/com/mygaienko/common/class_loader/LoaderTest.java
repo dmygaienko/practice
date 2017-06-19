18e8bcbafee3ee94b4327d864d7e1113ab7a5457
dmygaien <dmygaenko@gmail.com>
2017-06-20 00:08:54 +0300

    commit2.3.1.2

--- practice-common/src/test/java/com/mygaienko/common/class_loader/LoaderTest.java
+++ practice-common/src/test/java/com/mygaienko/common/class_loader/LoaderTest.java
@@ -1,8 +1,14 @@
 package com.mygaienko.common.class_loader;
 
+import com.mygaienko.common.stream.StreamUtil;
 import org.junit.Test;
+import org.springframework.instrument.classloading.ShadowingClassLoader;
 
 import java.lang.reflect.InvocationTargetException;
+
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by dmygaenko on 22/11/2016.
@@ -17,4 +23,16 @@ public class LoaderTest {
         loadClass.getConstructor().newInstance();
     }
 
+    @Test
+    public void testDynamicClassLoad() throws Exception {
+        Class<StreamUtil> aClass = StreamUtil.class;
+
+        ShadowingClassLoader shadowingClassLoader = new ShadowingClassLoader(aClass.getClassLoader());
+        Class<?> bClass = shadowingClassLoader.loadClass(aClass.getCanonicalName());
+
+        System.out.println(aClass.getClassLoader());
+        System.out.println(bClass.getClassLoader());
+
+        assertThat(aClass, not(sameInstance(bClass)));
+    }
 }
