f210cbbddb2b4eaa4a9be5e80d2bb2cd54f9cd30
dmygaien <dmygaenko@gmail.com>
2017-06-25 22:16:41 +0300

    commit2.3.1.2

--- practice-jpa/src/main/java/com/mygaienko/practice/jpa/dao/inheritance/tableperconcreteclass/SuperClassDao.java
+++ practice-jpa/src/main/java/com/mygaienko/practice/jpa/dao/inheritance/tableperconcreteclass/SuperClassDao.java
@@ -2,6 +2,7 @@ package com.mygaienko.practice.jpa.dao.inheritance.tableperconcreteclass;
 
 import com.mygaienko.practice.jpa.model.inheritance.tableperconcreteclass.B;
 import com.mygaienko.practice.jpa.model.inheritance.tableperconcreteclass.B_;
+import com.mygaienko.practice.jpa.model.inheritance.tableperconcreteclass.SuperClass;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -15,26 +16,18 @@ import java.util.List;
  * Created by enda1n on 25.06.2017.
  */
 @Repository
-public class SuperClass {
+public class SuperClassDao {
 
     @PersistenceContext
     private EntityManager entityManager;
 
-    public List<B> findAll() {
+    public List<SuperClass> findAll() {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<B> query = builder.createQuery(B.class);
-        Root<B> productRoot = query.from(B.class);
-        CriteriaQuery<B> allQuery = query.select(productRoot);
+        CriteriaQuery<SuperClass> query = builder.createQuery(SuperClass.class);
+        Root<SuperClass> productRoot = query.from(SuperClass.class);
+        CriteriaQuery<SuperClass> allQuery = query.select(productRoot);
 
         return entityManager.createQuery(allQuery).getResultList();
     }
 
-    public B get(Long id) {
-        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<B> query = builder.createQuery(B.class);
-        Root<B> productRoot = query.from(B.class);
-        query.where(builder.equal(productRoot.get(B_.id), id));
-
-        return entityManager.createQuery(query).getSingleResult();
-    }
 }
