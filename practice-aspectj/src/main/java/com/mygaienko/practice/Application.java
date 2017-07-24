b20120a9a5f9291ee39d43835a57d1fb4aa2b100
dmygaien <dmygaenko@gmail.com>
2017-07-25 00:19:34 +0300

    commit2.3.1.2

--- practice-aspectj/src/main/java/com/mygaienko/practice/Application.java
+++ practice-aspectj/src/main/java/com/mygaienko/practice/Application.java
@@ -2,11 +2,13 @@ package com.mygaienko.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
 
 /**
  * Created by enda1n on 24.07.2017.
  */
 @SpringBootApplication
+@EnableLoadTimeWeaving
 public class Application {
 
     public static void main(String[] args) {
