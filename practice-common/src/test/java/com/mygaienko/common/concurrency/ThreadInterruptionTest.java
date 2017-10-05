9cae809a62603885a898f73d93e0acabae7809df
dmygaien <dmygaenko@gmail.com>
2017-10-05 22:22:39 +0300

    commit2.3.1.2

--- practice-common/src/test/java/com/mygaienko/common/concurrency/ThreadInterruptionTest.java
+++ practice-common/src/test/java/com/mygaienko/common/concurrency/ThreadInterruptionTest.java
@@ -2,13 +2,15 @@ package com.mygaienko.common.concurrency;
 
 import org.junit.Test;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by enda1n on 05.10.2017.
  */
 public class ThreadInterruptionTest {
 
     @Test
-    public void test() throws Exception {
+    public void testInterruptionDuringSleep() throws Exception {
         Thread thread = new Thread(getRunnable());
         thread.start();
 
@@ -30,4 +32,35 @@ public class ThreadInterruptionTest {
             }
         };
     }
+
+    @Test
+    public void testInterruptionDuringWait() throws InterruptedException {
+        ReentrantLock lock = new ReentrantLock();
+        lock.lock();
+
+        Thread thread = new Thread(getRunnable(lock));
+        thread.start();
+
+       // Thread.sleep(1000);
+        thread.interrupt();
+        System.out.println("interrupted");
+        thread.join();
+
+        lock.unlock();
+    }
+
+    private Runnable getRunnable(ReentrantLock lock) {
+        return () -> {
+            System.out.println("waiting for lock");
+            try {
+                lock.lockInterruptibly();
+            } catch (InterruptedException e) {
+                System.out.println("interrupted");
+                return;
+            }
+            System.out.println("lock accepted");
+            lock.unlock();
+        };
+    }
+
 }
