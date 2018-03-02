3f37cb6bd93e7a4cba47f882a114a969b36d658b
dmygaienko <dmygaenko@gmail.com>
2018-03-02 15:06:51 +0200

    123

--- practice-common/src/test/java/com/mygaienko/common/text/StrSubstitutorTest.java
+++ practice-common/src/test/java/com/mygaienko/common/text/StrSubstitutorTest.java
@@ -19,4 +19,23 @@ public class StrSubstitutorTest {
         String expected = "Good morning, Dmytro. My pleasure to see you.";
         assertEquals(expected, StrSubstitutor.replace("${greeting}, ${name}. My pleasure to see you.", valueMap));
     }
+
+    @Test
+    public void testWithOneValue() {
+        Map<String, String> valueMap = new HashMap<>();
+        valueMap.put("greeting", "Good morning");
+
+        String expected = "Good morning, ${name}. My pleasure to see you.";
+        assertEquals(expected, StrSubstitutor.replace("${greeting}, ${name}. My pleasure to see you.", valueMap));
+    }
+
+    @Test
+    public void testWithNull() {
+        Map<String, String> valueMap = new HashMap<>();
+        valueMap.put("greeting", "Good morning");
+        valueMap.put("name", null);
+
+        String expected = "Good morning, ${name}. My pleasure to see you.";
+        assertEquals(expected, StrSubstitutor.replace("${greeting}, ${name}. My pleasure to see you.", valueMap));
+    }
 }
