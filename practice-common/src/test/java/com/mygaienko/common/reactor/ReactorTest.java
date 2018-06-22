387764e21354f6075116173b0d26ba32e6c8e6e8
Liuda Artysh <l.artysh@spd-ukraine.com>
2018-06-22 22:19:52 +0300

    reactor test

--- practice-common/src/test/java/com/mygaienko/common/reactor/ReactorTest.java
+++ practice-common/src/test/java/com/mygaienko/common/reactor/ReactorTest.java
@@ -2,14 +2,60 @@ package com.mygaienko.common.reactor;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.junit.Assert.assertThat;
 
 public class ReactorTest {
 
     @Test
-    public void test() {
+    public void testFlux() {
         Flux.just("1", "2", "3")
                 .filter(item -> item.equals("2"))
                 .subscribe(item -> System.out.println("item is " + item))
         ;
     }
+
+    @Test
+    public void testMono() {
+        Mono.just("1")
+                .filter(item -> !item.equals("2"))
+                .subscribe(item -> System.out.println("item is " + item))
+        ;
+    }
+
+    @Test
+    public void testMonoConcatWith() {
+        Mono.just("1").concatWith(Mono.just("2"))
+                .subscribe(item -> System.out.println("item is " + item))
+        ;
+    }
+
+    @Test
+    public void testCollectListAndBlock() {
+        List<String> list = Flux.just("1", "2")
+                .collectList()
+                .block()
+                ;
+
+        assertThat(list, contains("1", "2"));
+    }
+
+    @Test
+    public void testCollectMap() {
+        Map<String, String> map = Flux.just("1", "2")
+                .collectMap(Function.identity(), Function.identity())
+                .block()
+                ;
+
+        assertThat(map, hasEntry("1", "1"));
+        assertThat(map, hasEntry("2", "2"));
+    }
+
 }
