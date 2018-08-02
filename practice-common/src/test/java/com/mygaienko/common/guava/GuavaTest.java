c84a2a9b3a02e1f94452f39bfaee95d3a568fbfa
dmygaien <dmygaenko@gmail.com>
2018-08-02 21:57:49 +0300

    fourth simple step

--- practice-common/src/test/java/com/mygaienko/common/guava/GuavaTest.java
+++ practice-common/src/test/java/com/mygaienko/common/guava/GuavaTest.java
@@ -1,16 +1,16 @@
 package com.mygaienko.common.guava;
 
 import com.google.common.base.MoreObjects;
-import com.google.common.base.Objects;
 import com.google.common.base.Stopwatch;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.Range;
 import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class GuavaTest {
 
@@ -35,8 +35,34 @@ public class GuavaTest {
     }
 
     @Test
-    public void test() {
+    public void testFirstNonNull() {
         assertEquals(Long.valueOf(1L), MoreObjects.firstNonNull(null, 1L));
+    }
+
+    @Test
+    public void testCache() {
+        CacheLoader<String, String> loader = new CacheLoader<String, String>() {
+            @Override
+            public String load(String key) {
+                return key.toUpperCase();
+            }
+        };
+
+        LoadingCache<String, String> cache = CacheBuilder.newBuilder()
+                .maximumSize(3)
+//                .weigher((key, value) -> value.toString().length())
+                .expireAfterAccess(2, TimeUnit.MINUTES)
+                .refreshAfterWrite(2, TimeUnit.MINUTES)
+                .build(loader);
+
+        cache.getUnchecked("first");
+        cache.getUnchecked("second");
+        cache.getUnchecked("third");
+        cache.getUnchecked("forth");
+
+        assertEquals(3, cache.size());
+        assertNull(cache.getIfPresent("first"));
+        assertEquals("FORTH", cache.getIfPresent("forth"));
     }
 
 }
