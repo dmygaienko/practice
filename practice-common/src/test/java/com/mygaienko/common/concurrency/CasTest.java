a5cb9747927f425afc4ac4f9f15401841348ae7f
dmytro.mygaienko <dmygaenko@gmail.com>
2019-07-01 21:48:30 +0300

    add LongAdder

--- practice-common/src/test/java/com/mygaienko/common/concurrency/CasTest.java
+++ practice-common/src/test/java/com/mygaienko/common/concurrency/CasTest.java
@@ -8,49 +8,42 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.LongAdder;
+import java.util.function.Consumer;
 
 public class CasTest {
 
     @Test
     public void testAtomicInteger() throws InterruptedException {
-        StopWatch stopWatch = new StopWatch();
+        AtomicLong atomicLong = new AtomicLong();
 
-        AtomicLong atomicInteger = new AtomicLong();
+        StopWatch stopWatch = executeConcurrently(delta -> atomicLong.addAndGet(delta));
 
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-
-        stopWatch.start();
-        for (long i = 0; i < 1000000; i++) {
-            long finalI = i;
-            executorService.execute(() -> atomicInteger.addAndGet(finalI));
-        }
-
-        executorService.shutdown();
-        executorService.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
-        stopWatch.stop();
-
-        System.out.println(atomicInteger.get() + " in " + stopWatch.getTotalTimeSeconds());
+        System.out.println(atomicLong.get() + " in " + stopWatch.getTotalTimeSeconds());
     }
 
     @Test
     public void testLongAdder() throws InterruptedException {
-        StopWatch stopWatch = new StopWatch();
-
         LongAdder longAdder = new LongAdder();
 
+        StopWatch stopWatch = executeConcurrently(x -> longAdder.add(x));
+
+        System.out.println(longAdder.longValue() + " in " + stopWatch.getTotalTimeSeconds());
+    }
+
+    private StopWatch executeConcurrently(Consumer<Long> consumer) throws InterruptedException {
+        StopWatch stopWatch = new StopWatch();
         ExecutorService executorService = Executors.newFixedThreadPool(10);
 
         stopWatch.start();
         for (long i = 0; i < 1000000; i++) {
             long finalI = i;
-            executorService.execute(() -> longAdder.add(finalI));
+            executorService.execute(() -> consumer.accept(finalI));
         }
 
         executorService.shutdown();
         executorService.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
         stopWatch.stop();
-
-        System.out.println(longAdder.longValue() + " in " + stopWatch.getTotalTimeSeconds());
+        return stopWatch;
     }
 
 }
