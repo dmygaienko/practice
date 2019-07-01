b70e568a6bbbf7fb4312e3c17006292f43761d09
dmytro.mygaienko <dmygaenko@gmail.com>
2019-07-01 21:32:40 +0300

    add LongAdder

--- practice-common/src/test/java/com/mygaienko/common/concurrency/CasTest.java
+++ practice-common/src/test/java/com/mygaienko/common/concurrency/CasTest.java
@@ -1,28 +1,56 @@
 package com.mygaienko.common.concurrency;
 
 import org.junit.Test;
+import org.springframework.util.StopWatch;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
 
 public class CasTest {
 
     @Test
-    public void test() throws InterruptedException {
+    public void testAtomicInteger() throws InterruptedException {
+        StopWatch stopWatch = new StopWatch();
+
         AtomicInteger atomicInteger = new AtomicInteger();
 
         ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-        for (int i = 0; i < 1000; i++) {
+        stopWatch.start();
+        for (int i = 0; i < 100000; i++) {
             int finalI = i;
             executorService.execute(() -> atomicInteger.addAndGet(finalI));
         }
 
         executorService.shutdown();
         executorService.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
+        stopWatch.stop();
 
-        System.out.println(atomicInteger.get());
+        System.out.println(atomicInteger.get() + " in " + stopWatch.getTotalTimeSeconds());
     }
+
+    @Test
+    public void testLongAdder() throws InterruptedException {
+        StopWatch stopWatch = new StopWatch();
+
+        LongAdder longAdder = new LongAdder();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        stopWatch.start();
+        for (int i = 0; i < 100000; i++) {
+            int finalI = i;
+            executorService.execute(() -> longAdder.add(finalI));
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
+        stopWatch.stop();
+
+        System.out.println(longAdder.longValue() + " in " + stopWatch.getTotalTimeSeconds());
+    }
+
 }
