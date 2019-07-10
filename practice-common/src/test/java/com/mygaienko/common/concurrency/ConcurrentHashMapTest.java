2e974fb7fe3070e1fe319561c4b71a2567e3fd5f
dmytro.mygaienko <dmygaenko@gmail.com>
2019-07-10 20:10:25 +0300

    add LongAdder

---
+++ practice-common/src/test/java/com/mygaienko/common/concurrency/ConcurrentHashMapTest.java
@@ -0,0 +1,31 @@
+package com.mygaienko.common.concurrency;
+
+import org.junit.Test;
+
+import java.util.concurrent.*;
+
+public class ConcurrentHashMapTest {
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(6);
+
+    @Test
+    public void test() throws InterruptedException {
+        ConcurrentMap<Long, Long> map = new ConcurrentHashMap<>();
+
+        for (int i = 0; i < 20000; i++) {
+            long finalI = i;
+            executorService.execute(() -> map.compute(finalI, this::add));
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+        System.out.println(map);
+    }
+
+    private Long add(Long prev, Long next) {
+        Long result = 0L;
+        result = prev != null ? result + prev : result;
+        result = next != null ? result + next : result;
+        return result;
+    }
+}
