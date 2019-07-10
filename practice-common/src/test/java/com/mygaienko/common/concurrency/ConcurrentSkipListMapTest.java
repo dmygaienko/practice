df0e305f665c9f09f3c72f12ecd3c2beebfb8305
dmytro.mygaienko <dmygaenko@gmail.com>
2019-07-10 18:20:47 +0300

    add LongAdder

--- practice-common/src/test/java/com/mygaienko/common/concurrency/ConcurrentSkipListMapTest.java
+++ practice-common/src/test/java/com/mygaienko/common/concurrency/ConcurrentSkipListMapTest.java
@@ -2,9 +2,7 @@ package com.mygaienko.common.concurrency;
 
 import org.junit.Test;
 
-import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,17 +13,21 @@ public class ConcurrentSkipListMapTest {
 
     private ConcurrentSkipListMap<String, String> map = new ConcurrentSkipListMap<>();
 
-    private Executor executor = Executors.newFixedThreadPool(5);
+    private ExecutorService executor = Executors.newFixedThreadPool(5);
 
     @Test
-    public void test() {
+    public void test() throws InterruptedException {
         addValue("key1", "value1");
-        addValue("key2", "value2");
+        addValue("key7", "value7");
+        addValue("key6", "value3");
         addValue("key3", "value3");
         addValue("key4", "value4");
+        addValue("key2", "value2");
         addValue("key5", "value5");
+        executor.awaitTermination(5, TimeUnit.SECONDS);
 
         assertEquals("value1", map.ceilingEntry("key1").getValue());
+        System.out.println(map);
     }
 
     private void addValue(String key, String value) {
